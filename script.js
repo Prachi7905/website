22e65a73e8151be0d4d0e152f55c36ddb513e658
Prachi pandey <129525113+Prachi7905@users.noreply.github.com>
2023-07-14 23:16:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("Hello world");
+        //alert("Hiii");
+        document.write("This is me");
+    var number1=10;
+    var number2=12;
+    console.log(number1+number2);
+    var num1=56;
+    var num2=67.78;
+    console.log(num1+num2);
+    var str1= " This is string";
+    var str2= "This is also string";
+    console.log(str1+str2);
+    
+    var marks ={
+        ravi:76,
+        shubham:263,
+        asha:33,
+    }
+    console.log(marks);
+    var n=null;
+    console.log(n);
+    var a=100;
+    var b=10;
+    console.log("The value of a+b is :",a+b);
+    console.log("The value of a-b is :",a-b);
+    console.log("The value of a*b is :",a*b);
+    console.log("The value of a/b is :",a/b);
+    function avg(a,b){
+        c=(a+b)/2;
+        return c;
+    }
+     c1=avg(3,7);
+     c2=avg(2,6);
+     console.log(c1,c2);
